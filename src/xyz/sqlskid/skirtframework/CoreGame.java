3560234cdf5854ac07176fe03466d20ca8a7e52f
paimbot <perfaim12@gmail.com>
2022-02-03 19:00:35 +0100

    aaaa aaaaa

--- src/xyz/sqlskid/skirtframework/CoreGame.java
+++ src/xyz/sqlskid/skirtframework/CoreGame.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class CoreGame implements KeyListener, MouseListener, MouseMotionListener
 {
+    //commit test
     private int tickrate;
 
     private String title;
